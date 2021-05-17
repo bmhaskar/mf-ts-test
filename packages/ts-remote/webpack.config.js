b20bb6d6d5303ce8dfc91db2ce76054ccf190e0f
Bharat Mhaskar <bmhaskar@gmail.com>
2021-05-17 18:28:17 +0530

    Updated webpack to include the federated modules dynamically based on peerDependencies. Created versioned distribution files ready to publish based on module versions.

--- packages/ts-remote/webpack.config.js
+++ packages/ts-remote/webpack.config.js
@@ -1,68 +1,76 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
-const deps = require("./package.json").dependencies;
-module.exports = {
-  output: {
-    publicPath: "http://localhost:5000/",
-  },
-
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    port: 5000,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+const remoteURL = process.env.REMOTE_URL || "http://localhost:5000"
+const manifest = require("./package.json");
+module.exports = (env, argv)  => {
+  const libraryName = manifest.name.replace(/@|\/|-/g,'');
+  const config = {
+    output: {
+      publicPath: `${remoteURL}/${manifest.name}/${manifest.version}/`,
+      path: path.join(__dirname, 'dist', manifest.name, manifest.version)
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: libraryName,
+        filename: "remoteEntry.js",
+        library: {type: 'umd', 'name':  libraryName},
+        remotes: {},
+        exposes: {
+          "./ToDos": "./src/ToDos",
+          "./ToDoListView": "./src/ToDoListView"
+        },
+        shared: {
+          ...manifest.dependencies,
+          react: {
+            singleton: true,
+            requiredVersion: manifest.dependencies.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: manifest.dependencies["react-dom"],
+          }
+
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
+  }
 
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "tsremote",
-      filename: "remoteEntry.js",
-      library: { type: 'umd', 'name': 'tsremote'},
-      remotes: {},
-      exposes: {
-        "./ToDos": "./src/ToDos",
-        "./ToDoListView":"./src/ToDoListView"
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        }
-
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  if(argv.mode == "development") {
+    config.devtool =  "eval-source-map";
+    config.devServer = {
+      port: 3001
+    }
+    config.output.publicPath = `http://localhost:3001/${manifest.name.replace('@swsl/','')}`;
+  }
+  return config;
 };
